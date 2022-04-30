a8b8456af13905fa5c7b154d4aa2f24ea016254b
MarkLee <1643565650@qq.com>
2022-04-30 10:47:27 +0800

    引入svg，封装为<icon>组件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,29 @@
+// 添加模块
+const path = require("path");
+
 module.exports = {
-  lintOnSave: false
-}
+  lintOnSave: false,
+  chainWebpack: (config) => {
+    // 确定icons所在的目录，定义为dir（也就是director目录的意思）
+    const dir = path.resolve(__dirname, "src/assets/icons");
+
+    config.module
+      .rule("svg-sprite")
+
+      .test(/\.svg$/) // 如果匹配上这个正则，就用这个规则
+
+      .include.add(dir) // 只在dir目录下的svg走这个规则
+      .end() // 只包含 icons 目录
+
+      .use("svg-sprite-loader") // 使用哪些loader
+      .loader("svg-sprite-loader")
+
+      .options({ extract: false }) // 不要把它解析出文件
+      .end();
+    config
+      // 配置插件
+      .plugin("svg-sprite")
+      .use(require("svg-sprite-loader/plugin"), [{ plainSprite: true }]);
+    config.module.rule("svg").exclude.add(dir); // 其他 svg loader 排除 icons 目录
+  },
+};
